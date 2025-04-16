b253c1185e8777492476afbfa1600025bfd09994
edwarddavis1 <edwarddavis@hotmail.co.uk>
2025-04-16 14:04:35 +0100

    added tests for the get_prediction_sets_function

---
+++ tests/test_conformal.py
@@ -0,0 +1,50 @@
+import pytest
+import numpy as np
+import torch
+from torch_geometric.data import Data
+
+from ugnn.conformal import get_prediction_sets
+
+
+def test_get_prediction_sets_valid_inputs():
+    output = torch.tensor([[2.0, 1.0], [0.5, 1.5], [1.0, 2.0], [1.5, 0.5]])
+    data = Data(
+        y=torch.tensor([1, 0, 0, 1]),
+    )
+    calib_mask = torch.tensor([True, True, True, False])
+    test_mask = torch.tensor([False, False, False, True])
+    pred_sets = get_prediction_sets(
+        output, data, calib_mask, test_mask, alpha=0.7, method="APS"
+    )
+
+    assert pred_sets.shape == (1, 2)  # Ensure correct shape
+    assert pred_sets.dtype == np.bool_  # Ensure binary output
+
+
+def test_get_prediction_sets_bad_quantile():
+
+    # Same example, but with low n_calibration and low alpha
+    # This means we want the m-th quantile of non-conformities but m > 1
+
+    output = torch.tensor([[2.0, 1.0], [0.5, 1.5], [1.0, 2.0], [1.5, 0.5]])
+    data = Data(
+        y=torch.tensor([1, 0, 0, 1]),
+    )
+    calib_mask = torch.tensor([True, True, True, False])
+    test_mask = torch.tensor([False, False, False, True])
+
+    with pytest.raises(ValueError, match="Specified quantile is larger than 1"):
+        get_prediction_sets(
+            output, data, calib_mask, test_mask, alpha=0.1, method="APS"
+        )
+
+
+def test_get_prediction_sets_unsupported_method():
+    output = torch.tensor([[2.0, 1.0]])
+    data = type("Data", (), {"y": torch.tensor([0])})
+    calib_mask = torch.tensor([True])
+    test_mask = torch.tensor([False])
+    with pytest.raises(ValueError, match="Unknown method: INVALID"):
+        get_prediction_sets(
+            output, data, calib_mask, test_mask, alpha=0.1, method="INVALID"
+        )
--- ugnn/conformal.py
+++ ugnn/conformal.py
@@ -1,8 +1,19 @@
-import torch
 import numpy as np
+import torch
+
+from typing import Literal
+from torch import Tensor
+from torch_geometric.data import Data
 
 
-def get_prediction_sets(output, data, calib_mask, test_mask, alpha=0.1, method="APS"):
+def get_prediction_sets(
+    output: Tensor,
+    data: Data,
+    calib_mask: Tensor,
+    test_mask: Tensor,
+    alpha: float = 0.1,
+    method: Literal["APS"] = "APS",
+) -> np.ndarray[np.bool_]:
     """
     Takes the output of a model and computes the conformal prediction sets.
 
@@ -14,13 +25,14 @@ def get_prediction_sets(output, data, calib_mask, test_mask, alpha=0.1, method="
     - APS: Adaptive Prediction Sets
 
     """
-    n_calib = calib_mask.sum()
+    n_calib = calib_mask.sum().item()
 
     # Compute softmax probabilities
     smx = torch.nn.Softmax(dim=1)
     calib_heuristic = smx(output[calib_mask]).detach().numpy()
     test_heuristic = smx(output[test_mask]).detach().numpy()
 
+    # Compute non-conformity scores
     if method == "APS":
         calib_pi = calib_heuristic.argsort(1)[:, ::-1]
         calib_srt = np.take_along_axis(calib_heuristic, calib_pi, axis=1).cumsum(axis=1)
@@ -33,10 +45,16 @@ def get_prediction_sets(output, data, calib_mask, test_mask, alpha=0.1, method="
         raise ValueError(f"Unknown method: {method}")
 
     # Get the score quantile
-    qhat = np.quantile(
-        calib_scores, np.ceil((n_calib + 1) * (1 - alpha)) / n_calib, method="higher"
-    )
+    qhat_quantile = np.ceil((n_calib + 1) * (1 - alpha)) / n_calib
 
+    if qhat_quantile > 1:
+        raise ValueError(
+            f"Specified quantile is larger than 1. Either increase the number of calibration data points or increase alpha."
+        )
+
+    qhat = np.quantile(calib_scores, qhat_quantile, method="higher")
+
+    # Return the prediction sets
     test_pi = test_heuristic.argsort(1)[:, ::-1]
     test_srt = np.take_along_axis(test_heuristic, test_pi, axis=1).cumsum(axis=1)
     pred_sets = np.take_along_axis(test_srt <= qhat, test_pi.argsort(axis=1), axis=1)
