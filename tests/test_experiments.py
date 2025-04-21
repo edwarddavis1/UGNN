0e3de838a0f1610f17cc694d5240dc60550d9da6
edwarddavis1 <edwarddavis@hotmail.co.uk>
2025-04-21 16:16:41 +0100

    added experiment argparser tests

--- tests/test_experiments.py
+++ tests/test_experiments.py
@@ -1,8 +1,11 @@
+import pytest
+import sys
 import numpy as np
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
+from ugnn.types import AdjacencySeries
 from ugnn.experiment_config import SBM_EXPERIMENT_PARAMS
-from ugnn.experiments import Experiment
+from ugnn.experiments import Experiment, parse_args_load_data
 
 
 def test_experiment_initialisation():
@@ -35,3 +38,39 @@ def test_experiment_initialisation():
     )
 
     assert isinstance(experiment, Experiment)
+
+
+@pytest.mark.parametrize("data", ["school", "sbm", "flight"])
+def test_parse_args_load_data(data):
+
+    test_args = [
+        "run_conformal_experiment.py",
+        "--data",
+        data,
+        "--debug",
+        "--name",
+        "test_run",
+        "--regime",
+        "semi-inductive",
+        "--conformal_method",
+        "RAPS",
+        "--method",
+        "BD",
+        "--GNN",
+        "GCN",
+    ]
+
+    with patch.object(sys, "argv", test_args):
+        exp_params, data_params = parse_args_load_data()
+
+    # Assertions
+    assert exp_params["experiment_name"] == "test_run_debug"
+    assert exp_params["regimes"] == ["semi-inductive"]
+    assert exp_params["conformal_method"] == "RAPS"
+    assert exp_params["methods"] == ["BD"]
+    assert exp_params["GNN_models"] == ["GCN"]
+
+    assert data_params["T"] > 0
+    assert data_params["n"] > 0
+    assert isinstance(data_params["As"], AdjacencySeries)
+    assert isinstance(data_params["node_labels"], np.ndarray)
