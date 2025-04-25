a6fe305b38724b6cc90178802ccf73cf288c4db2
edwarddavis1 <edwarddavis@hotmail.co.uk>
2025-04-25 06:46:07 +0100

    updated the example output

--- experiments/run_all_experiments.sh
+++ experiments/run_all_experiments.sh
@@ -1,3 +1,3 @@
-uv run experiments/run_conformal_experiment.py --data sbm
-uv run experiments/run_conformal_experiment.py --data school
-uv run experiments/run_conformal_experiment.py --data flight
+uv run experiments/run_conformal_experiment.py --data sbm --name full_run
+uv run experiments/run_conformal_experiment.py --data school --name full_run
+uv run experiments/run_conformal_experiment.py --data flight --name full_run
